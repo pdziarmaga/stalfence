73de8e6752dc8c5d8a26eca1ef8463261c6b0ce4
Pawel Dziarmaga <dziarmaga.p@gmail.com>
2021-01-18 23:27:37 +0100

    added realizations section with gallery

---
+++ stalfence/js/gallery.js
@@ -0,0 +1,133 @@
+$(document).ready(function () {  
+     $('#collapseTwo').collapse({toggle: true});
+     $('#collapseThree').collapse({toggle: true})
+     $('#collapseFour').collapse({toggle: true})
+     $('#collapseFive').collapse({toggle: true})
+     $('#collapseSix').collapse({toggle: true})
+   });
+
+$(document).ready(function () {
+    $('#gallery-wrought').justifiedGallery({
+        rowHeight: 110,
+        maxRowHeight: null,
+        margins: 3,
+        border: 3,
+        rel: 'liveDemo',
+        lastRow: 'nojustify',
+        captions: false,
+        randomize: false,
+        sizeRangeSuffixes: {
+            lt100: '_m',//t
+            lt240: '_m',//m
+            lt320: '_m',//n
+            lt500: '',
+            lt640: '_m',//z
+            lt1024: '_m'//b
+        }
+    });
+});
+
+$(document).ready(function () {
+    $('#gallery-modern').justifiedGallery({
+        rowHeight: 110,
+        maxRowHeight: null,
+        margins: 3,
+        border: 3,
+        rel: 'liveDemo',
+        lastRow: 'nojustify',
+        captions: false,
+        randomize: false,
+        sizeRangeSuffixes: {
+            lt100: '_m',//t
+            lt240: '_m',//m
+            lt320: '_m',//n
+            lt500: '',
+            lt640: '_m',//z
+            lt1024: '_m'//b
+        }
+    })
+});
+
+$(document).ready(function () {
+    $('#gallery-industrial').justifiedGallery({
+        rowHeight: 110,
+        maxRowHeight: null,
+        margins: 3,
+        border: 3,
+        rel: 'liveDemo',
+        lastRow: 'nojustify',
+        captions: false,
+        randomize: false,
+        sizeRangeSuffixes: {
+            lt100: '_m',//t
+            lt240: '_m',//m
+            lt320: '_m',//n
+            lt500: '',
+            lt640: '_m',//z
+            lt1024: '_m'//b
+        }
+    })
+});
+
+$(document).ready(function () {
+    $('#gallery-mesh').justifiedGallery({
+        rowHeight: 110,
+        maxRowHeight: null,
+        margins: 3,
+        border: 3,
+        rel: 'liveDemo',
+        lastRow: 'nojustify',
+        captions: false,
+        randomize: false,
+        sizeRangeSuffixes: {
+            lt100: '_m',//t
+            lt240: '_m',//m
+            lt320: '_m',//n
+            lt500: '',
+            lt640: '_m',//z
+            lt1024: '_m'//b
+        }
+    })
+});
+
+$(document).ready(function () {
+    $('#gallery-balcony').justifiedGallery({
+        rowHeight: 110,
+        maxRowHeight: null,
+        margins: 3,
+        border: 3,
+        rel: 'liveDemo',
+        lastRow: 'nojustify',
+        captions: false,
+        randomize: false,
+        sizeRangeSuffixes: {
+            lt100: '_m',//t
+            lt240: '_m',//m
+            lt320: '_m',//n
+            lt500: '',
+            lt640: '_m',//z
+            lt1024: '_m'//b
+        }
+    })
+});
+
+$(document).ready(function () {
+    $('#gallery-automation').justifiedGallery({
+        rowHeight: 110,
+        maxRowHeight: null,
+        margins: 3,
+        border: 3,
+        rel: 'liveDemo',
+        lastRow: 'nojustify',
+        captions: false,
+        randomize: false,
+        sizeRangeSuffixes: {
+            lt100: '_m',//t
+            lt240: '_m',//m
+            lt320: '_m',//n
+            lt500: '',
+            lt640: '_m',//z
+            lt1024: '_m'//b
+        }
+    })
+});
